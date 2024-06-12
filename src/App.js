218d8bd7bb6f6c9fbd57e5818d3d216599762c05
hannasherin <hannasherin7110@gmail.com>
2024-06-13 04:00:40 +0530

    message

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { AddPatient } from './components/AddPatient';
 import { ViewAll } from './components/ViewAll';
 import { SearchPatient } from './components/SearchPatient';
+import { DeletePatient } from './components/DeletePatient';
 
 function App() {
   return (
     <div className="App">
       <AddPatient></AddPatient>
       <SearchPatient></SearchPatient>
+      <DeletePatient></DeletePatient>
       <ViewAll></ViewAll>
     </div>
   );
