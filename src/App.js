1ced2e2f35f4e63349d58f76302022378b6628f7
hannasherin <hannasherin7110@gmail.com>
2024-06-13 03:56:32 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { AddPatient } from './components/AddPatient';
 import { ViewAll } from './components/ViewAll';
+import { SearchPatient } from './components/SearchPatient';
 
 function App() {
   return (
     <div className="App">
       <AddPatient></AddPatient>
+      <SearchPatient></SearchPatient>
       <ViewAll></ViewAll>
     </div>
   );
---
+++ src/components/SearchPatient.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+
+import axios from 'axios'
+
+export const SearchPatient = () => {
+    const [data,setData]=useState(
+        {
+          "name":""
+        }
+      )
+
+
+     const [result,setResult]=useState([])
+
+      const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+      }
+      const readVlue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8089/search",data).then(
+          (response)=>{
+            setResult(response.data)
+            
+          }
+        ).catch(
+          (error)=>{
+            console.log(error.message)
+            alert(error.map)
+          }
+        ).finally()
+      }
+
+  return (
+    <div>
+        <h1><center>SEARCH PATIENT</center></h1>
+     
+       <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">patient name</label>
+                        <input type="text" className="form-control"name='name'value={data.name}onChange={inputHandler}></input>
+                    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <br /><br /><button className="btn btn-warning" onClick={readVlue}>search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row g-3">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table" border="1">
+                    <thead>
+                      <tr>
+                        <th scope="col">patient name</th>
+                        <th scope="col">mobile no</th>
+                        <th scope="col">id</th>
+                        <th scope="col">blood group</th>
+                      </tr>
+                    </thead>
+                    
+                    {result.map(
+                        (value,index)=>
+                            {
+                                return <tbody>
+                                <tr>
+                                  <td>{value.name}</td>
+                                  <td>{value.mobno}</td>
+                                  <td>{value.id}</td>
+                                  <td>{value.blood}</td>
+                                </tr>
+                                </tbody>
+                            }
+                    )}
+                  </table>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    
+  )
+}
