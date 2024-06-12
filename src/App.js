d84261427fc64118c486f87af5038f2fdc1726e3
hannasherin <hannasherin7110@gmail.com>
2024-06-13 03:51:47 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { AddPatient } from './components/AddPatient';
+import { ViewAll } from './components/ViewAll';
 
 function App() {
   return (
     <div className="App">
       <AddPatient></AddPatient>
+      <ViewAll></ViewAll>
     </div>
   );
 }
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+
+import axios from 'axios'
+
+export const ViewAll = () => {
+  const [data,changeData]=useState([])
+  const fetchData=()=>{
+      axios.get("http://localhost:8089/view").then(
+          (response)=>{
+              changeData(response.data)
+          }
+      ).catch(
+        (error)=>{
+          console.log(error.message)
+          alert(error.map)
+        }
+      ).finally()
+  }
+  useEffect(()=>{fetchData()},[])
+  return (
+    <div>
+      <h1><center>VIEW PATIENT DETAILS</center></h1>
+        
+         <div class="container">
+        <div class="row g-3">
+            <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <table class="table" border="1">
+                    <thead>
+                      <tr>
+                        <th scope="col">patient name</th>
+                        <th scope="col">mobile no</th>
+                        <th scope="col">id</th>
+                        <th scope="col">blood group</th>
+                      </tr>
+                    </thead>
+                    
+                    {data.map(
+                        (value,index)=>
+                            {
+                                return <tbody>
+                                <tr>
+                                  <td>{value.name}</td>
+                                  <td>{value.mobno}</td>
+                                  <td>{value.id}</td>
+                                  <td>{value.blood}</td>
+                                </tr>
+                                </tbody>
+                            }
+                    )}
+                  </table>
+            </div>
+        </div>
+    </div>
+
+    </div>
+  )
+}
